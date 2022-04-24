13ad301b515a50048ca84c3898041db90d309542
lidaye910416 <437147838@qq.com>
2022-04-24 15:19:47 +0800

    成功跑起来样例代码-220424

---
+++ src/views/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="container"></div>
+</template>
+<script>
+import * as Cesium from "cesium/Cesium";
+import * as widgets from "cesium/Widgets/widgets.css";
+export default {
+  name: 'HelloCesium',
+  mounted () {
+    const viewer = new Cesium.Viewer('container');
+  }
+}
+</script>
+<style scoped lang="scss">
+#container {
+  width: 100%;
+  height: 100%;
+}
+</style>
